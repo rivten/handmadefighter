4764904965d02c817503710308e8fc64b852b989
rivten <rivten.grey@gmail.com>
2016-11-15 22:35:12 +0100

    add hitbox detection on left/right of the fighter

--- code/fighter.gd
+++ code/fighter.gd
@@ -1,16 +1,39 @@
 
-extends Node2D
+extends KinematicBody2D
 
 export var AccelerationNorm = 6000
 export var Drag = 10
 var Velocity = Vector2(0, 0)
 var InitPos = Vector2(0, 0)
+var DrawLeft = false
+var DrawRight = false
 
 export(Vector2) var BulletVelocity = Vector2(90.0, 0.0)
+
+func _draw():
+	var LeftAreaNodeRect = find_node("LeftArea").get_item_rect()
+	var RightAreaNodeRect = find_node("RightArea").get_item_rect()
+	LeftAreaNodeRect.size.y = 0.5 * LeftAreaNodeRect.size.y
+	RightAreaNodeRect.pos = LeftAreaNodeRect.pos + Vector2(0, LeftAreaNodeRect.size.y)
+	RightAreaNodeRect.size.y = 0.5 * RightAreaNodeRect.size.y
+	if(DrawRight):
+		self.draw_rect(RightAreaNodeRect, Color(1.0, 0.0, 0.0, 1.0))
+	else:
+		self.draw_rect(RightAreaNodeRect, Color(0.0, 1.0, 0.0, 1.0))
+
+	if(DrawLeft):
+		self.draw_rect(LeftAreaNodeRect, Color(1.0, 0.0, 0.0, 1.0))
+	else:
+		self.draw_rect(LeftAreaNodeRect, Color(0.0, 1.0, 0.0, 1.0))
 
 func _ready():
 	set_fixed_process(true)
 	set_pos(InitPos)
+
+	var LeftAreaNode = find_node("LeftArea")
+	var RightAreaNode = find_node("RightArea")
+	LeftAreaNode.connect("area_enter", self, "draw_left_area")
+	RightAreaNode.connect("area_enter", self, "draw_right_area")
 
 func shoot():
 	var Root = get_tree().get_root().get_node("Game")
@@ -34,5 +57,19 @@ func _fixed_process(dt):
 	var DeltaPos = dt * Velocity + 0.5 * dt * dt * Acceleration
 
 	move(DeltaPos)
+	self.update()
 
+func draw_left_area(AreaEntered):
+	if(DrawLeft):
+		DrawLeft = false
+	else:
+		DrawLeft = true
+	self.update()
+
+func draw_right_area(AreaEntered):
+	if(DrawRight):
+		DrawRight = false
+	else:
+		DrawRight = true
+	self.update()
 
