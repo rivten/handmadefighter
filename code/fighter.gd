3d0e7a2be4a078c4ffd2c16089a9ae5212d495be
rivten <rivten.grey@gmail.com>
2016-11-15 01:28:12 +0100

    add ability for fighter to shoot bullet at given velocity

--- code/fighter.gd
+++ code/fighter.gd
@@ -6,9 +6,18 @@ export var Drag = 10
 var Velocity = Vector2(0, 0)
 var InitPos = Vector2(0, 0)
 
+export(Vector2) var BulletVelocity = Vector2(90.0, 0.0)
+
 func _ready():
 	set_fixed_process(true)
 	set_pos(InitPos)
+
+func shoot():
+	var Root = get_tree().get_root().get_node("Game")
+	var BulletNode = preload("res://Bullet.tscn").instance()
+	BulletNode.set_pos(get_pos())
+	BulletNode.set_velocity(BulletVelocity)
+	Root.add_child(BulletNode)
 
 func _fixed_process(dt):
 	var Acceleration = Vector2(0, 0)
@@ -17,6 +26,8 @@ func _fixed_process(dt):
 		Acceleration += Vector2(0, -1)
 	if(Input.is_action_pressed("down")):
 		Acceleration += Vector2(0, 1)
+	if(Input.is_action_pressed("right")):
+		shoot()
 
 	Acceleration *= AccelerationNorm
 	Velocity += dt * Acceleration - dt * Drag * Velocity
