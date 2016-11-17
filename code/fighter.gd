6aa817d6d6c764a5e4bc5b8c11f60550ab9eebe2
khoadot <khoa.dotrinh@gmail.com>
2016-11-17 22:54:56 +0100

    projection when hit

--- code/fighter.gd
+++ code/fighter.gd
@@ -4,71 +4,52 @@ extends KinematicBody2D
 export var AccelerationNorm = 6000
 export var Drag = 10
 export var HitlagTeleportDelta = 100
-var Velocity = Vector2(0, 0)
+export var ProjectionAccelerationNorm = 100000
 var InitPos = Vector2(0, 0)
-var DrawLeft = false
-var DrawRight = false
-var Freezed = false
+var Velocity = Vector2(0, 0)
+var Acceleration = Vector2(0.0, 0.0)
+var Frozen = false
 var FreezeInputRecorded = false
 var FreezeInput = Vector2(0.0, 0.0)
 var FreezeTimerNode
+var ProjectionAcceleration = Vector2(0.0, 0.0)
+var LastHitSide
 
 export(Vector2) var BulletVelocity = Vector2(90.0, 0.0)
-
-func _draw():
-	var LeftAreaNodeRect = find_node("LeftArea").get_item_rect()
-	var RightAreaNodeRect = find_node("RightArea").get_item_rect()
-	LeftAreaNodeRect.size.y = 0.5 * LeftAreaNodeRect.size.y
-	RightAreaNodeRect.pos = LeftAreaNodeRect.pos + Vector2(0, LeftAreaNodeRect.size.y)
-	RightAreaNodeRect.size.y = 0.5 * RightAreaNodeRect.size.y
-	if(DrawRight):
-		self.draw_rect(RightAreaNodeRect, Color(1.0, 0.0, 0.0, 1.0))
-	else:
-		self.draw_rect(RightAreaNodeRect, Color(0.0, 1.0, 0.0, 1.0))
-
-	if(DrawLeft):
-		self.draw_rect(LeftAreaNodeRect, Color(1.0, 0.0, 0.0, 1.0))
-	else:
-		self.draw_rect(LeftAreaNodeRect, Color(0.0, 1.0, 0.0, 1.0))
 
 func _ready():
 	set_fixed_process(true)
 	set_pos(InitPos)
 
-	Freezed = false
+	Frozen = false
 	FreezeInputRecorded = false
 	FreezeInput = Vector2(0.0, 0.0)
 
 	FreezeTimerNode = find_node("FreezeTimer")
+	FreezeTimerNode.connect("timeout", self, "teleport_and_project")
 
 	var LeftAreaNode = find_node("LeftArea")
 	var RightAreaNode = find_node("RightArea")
-	LeftAreaNode.connect("area_enter", self, "hit_left_area")
-	RightAreaNode.connect("area_enter", self, "hit_right_area")
-	FreezeTimerNode.connect("timeout", self, "end_freeze")
+	LeftAreaNode.connect("area_enter", self, "set_hit_side_to_left")
+	RightAreaNode.connect("area_enter", self, "set_hit_side_to_right")
 
-func shoot():
-	var Root = get_tree().get_root().get_node("Game")
-	var BulletNode = preload("res://Bullet.tscn").instance()
-	BulletNode.set_pos(get_pos())
-	BulletNode.set_velocity(BulletVelocity)
-	Root.add_child(BulletNode)
 
 func _fixed_process(dt):
-	var Acceleration = Vector2(0, 0)
 
-	if(!Freezed):
+	if(!Frozen):
 		if(Input.is_action_pressed("up")):
-			Acceleration += Vector2(0, -1)
+			Acceleration += AccelerationNorm * Vector2(0, -1)
 		if(Input.is_action_pressed("down")):
-			Acceleration += Vector2(0, 1)
+			Acceleration += AccelerationNorm * Vector2(0, 1)
 		if(Input.is_action_pressed("right")):
 			shoot()
 
-		Acceleration *= AccelerationNorm
 		Velocity += dt * Acceleration - dt * Drag * Velocity
 		var DeltaPos = dt * Velocity + 0.5 * dt * dt * Acceleration
 
+		Acceleration = Vector2(0.0, 0.0)
+		#(K) Acceleration is reset at the end (and not the beginning) so that another 
+		# function (like teleport_and_project) can decide of the acceleration for one frame
 		move(DeltaPos)
 
 	else : #NOTE(hugo) : if Freeze
@@ -80,29 +61,41 @@ func _fixed_process(dt):
 				FreezeInput = Vector2(0, 1)
 				FreezeInputRecorded = true
 
-	self.update()
+func shoot():
+	var Root = get_tree().get_root().get_node("Game")
+	var BulletNode = preload("res://Bullet.tscn").instance()
+	BulletNode.set_pos(get_pos())
+	BulletNode.set_velocity(BulletVelocity)
+	Root.add_child(BulletNode)
 
-func hit_left_area(AreaEntered):
-	if(!Freezed):
-		Freezed = true
+func set_hit_side_to_left(EnteredHitbox):
+	EnteredHitbox.queue_free()
+	if(!Frozen):
+		Frozen = true
 		FreezeTimerNode.start()
+		LastHitSide = "left"
 
-	DrawLeft = !DrawLeft
-	self.update()
-
-func hit_right_area(AreaEntered):
-	if(!Freezed):
-		Freezed = true
+func set_hit_side_to_right(EnteredHitbox):
+	EnteredHitbox.queue_free()
+	if(!Frozen):
+		Frozen = true
 		FreezeTimerNode.start()
+		LastHitSide = "right"
 
-	DrawRight = !DrawRight
-	self.update()
-
-func end_freeze():
+func teleport_and_project():
+	#(K)Teleport…
 	var DeltaFreezePos = HitlagTeleportDelta * FreezeInput;
 	move(DeltaFreezePos)
 
+	#(K)…and project
+	var HitDirection
+	if (LastHitSide == "left"):
+		HitDirection = Vector2(0, 1)
+	else: # LastHitSide == "right"
+		HitDirection = Vector2(0, -1)
+	Acceleration += ProjectionAccelerationNorm * HitDirection
+
 	# NOTE(hugo): re-init of freeze parameters
 	FreezeInput = Vector2(0, 0)
-	Freezed = false
+	Frozen = false
 	FreezeInputRecorded = false
--- code/static_hitbox.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-func _ready():
-	set_fixed_process(true)
-	
-func _fixed_process(delta):
-	if (!get_overlapping_bodies().empty()):
-		self.queue_free()
