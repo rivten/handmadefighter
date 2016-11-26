b07fd316aa77a1746c173837a625dd57533989a2
rivten <rivten.grey@gmail.com>
2016-11-26 19:21:12 +0100

    Make hit projection proportional to hit power

--- code/fighter.gd
+++ code/fighter.gd
@@ -12,7 +12,8 @@ var InputMap = ["", "", "", "", ""]
 export var AccelerationNorm = 6000
 export var Drag = 10
 export var HitlagTeleportDelta = 100
-export var ProjectionAccelerationNorm = 100000
+export var PitchAxisProjectionBaseIntensity = 100000
+export var RollAxisProjectionBaseIntensity = 10000
 export(float, 0.0, 2.0, 0.01) var CooldownDuration = 0.2
 export(float, 0.0, 500.0, 10.0) var HomelineAttractionSpeed = 1.0
 var InitPos = Vector2(0, 0)
@@ -25,6 +26,7 @@ var FreezeTimerNode
 var CooldownTimerNode
 var ProjectionAcceleration = Vector2(0.0, 0.0)
 var LastHitSide
+var LastHitDamage
 var IsControllable = true
 
 # NOTE(hugo) : Shoot data
@@ -163,17 +165,21 @@ func start_hitlag_from_left(EnteredHitbox):
 func start_hitlag_from_right(EnteredHitbox):
 	start_hitlag(EnteredHitbox, "right")
 
+# TODO(hugo):  A quick debug make me notice that we go through this function way too many times :
+#   (A) the fighter who shoot actually collides with its bullet, so we go in there (but do nothing fortunately)
+#   (B) if the bullet hits the middle of the other fighter, we call this for each side. I don't even understand why we are not computing the damage twice in this case
 func start_hitlag(EnteredHitbox, HitSide):
 	var Damage = 0
 	if(!EnteredHitbox.is_in_group(BulletsGroupName)):
 		#NOTE(hugo): I don't really like that but I don't see any easy way around this. We need to get the power of a bullet, so we need to know if we hit a bullet first, then react accordingly
-		if(EnteredHitbox.get_name() == "Bullet"):
+		if("Bullet".is_subsequence_of(EnteredHitbox.get_name())):
 			Damage = EnteredHitbox.Power
 		EnteredHitbox.queue_free()
 		if(!Frozen):
 			Frozen = true
 			FreezeTimerNode.start()
 			LastHitSide = HitSide
+			LastHitDamage = Damage
 			emit_signal("hit_by_hitbox", self.get_name(), Damage)
 
 func teleport_and_project():
@@ -182,12 +188,20 @@ func teleport_and_project():
 	move(DeltaFreezePos)
 
 	#(K)…and project
-	var HitDirection
+	#(K) I am using the vocabulary of pitch (left/right), roll(front/rear) and yaw(sky/ground) axis 
+
+	var PitchAxisAcceleration
+	print(LastHitDamage)
+	print(BulletDir)
+	var RollAxisAcceleration = RollAxisProjectionBaseIntensity * LastHitDamage * BulletDir
 	if (LastHitSide == "left"):
-		HitDirection = Vector2(0, 1) + 0.5 * BulletDir
+		PitchAxisAcceleration = PitchAxisProjectionBaseIntensity * Vector2(0, 1)
+	#	HitDirection = Vector2(0, 1) + 0.5 * BulletDir
 	else: # LastHitSide == "right"
-		HitDirection = Vector2(0, -1) + 0.5 * BulletDir
-	Acceleration += ProjectionAccelerationNorm * HitDirection
+		PitchAxisAcceleration = PitchAxisProjectionBaseIntensity * Vector2(0, -1)
+		
+	#	HitDirection = Vector2(0, -1) + 0.5 * BulletDir
+	Acceleration += PitchAxisAcceleration + RollAxisAcceleration
 
 	# NOTE(hugo): re-init of freeze parameters
 	FreezeInput = Vector2(0, 0)
