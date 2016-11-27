cd08f36c35fbd668f023852831e9f57f0b55a25f
rivten <rivten.grey@gmail.com>
2016-11-27 12:57:35 +0100

    60 draw parabolic death line for left fighter

--- code/game.gd
+++ code/game.gd
@@ -37,6 +37,22 @@ func instanciate_right_hitbox():
 	RightHitbox.set_pos(RightHitboxInitialPos)
 #################################
 
+func deathline_equation(Y, WindowSize):
+	# NOTE(hugo) : This is where there is a problem with Godot. I would like to get rid of the WindowSize in this equation.
+	# The deathline should _not_ be dependant on the screen current resolution. Me is sad :(
+	return(0.75 * WindowSize.x - 0.002 * (Y - 0.5 * WindowSize.y) * (Y - 0.5 * WindowSize.y))
+
+func _draw():
+	var WindowSize = get_viewport().get_rect().size
+	var StepCount = 100
+	var Epsilon = WindowSize.y / StepCount
+	for StepIndex in range(StepCount):
+		var YBegin = StepIndex * Epsilon
+		var YEnd = (StepIndex + 1) * Epsilon
+		var XBegin = deathline_equation(YBegin, WindowSize)
+		var XEnd = deathline_equation(YEnd, WindowSize)
+		draw_line(Vector2(XBegin, YBegin), Vector2(XEnd, YEnd), Color(236, 88, 0), 1)
+
 func _ready():
 	set_fixed_process(false)
 	var WindowSize = get_viewport().get_rect().size
